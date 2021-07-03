ddf57bfc7d3da2b4b1685d2eb427daa4d175a603
BilalRehman08 <bilalrehman080808@gmail.com>
2021-07-03 03:16:03 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+function signup() {
+    var name = document.getElementById("name");
+    var email = document.getElementById("email");
+    var password = document.getElementById("password");
+    var contact = document.getElementById("phone");
+    var address = document.getElementById("address");
+}
+
+function login() {
+    var email = document.getElementById("email");
+    var password = document.getElementById("password");
+}
