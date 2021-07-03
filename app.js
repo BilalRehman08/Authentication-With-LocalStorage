a7f4a147a0d78daef10875fad0004d4a916b28eb
BilalRehman08 <bilalrehman080808@gmail.com>
2021-07-03 04:01:02 -0700

    first commit

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ function signup() {
     var password = document.getElementById("password");
     var contact = document.getElementById("phone");
     var address = document.getElementById("address");
+    var message = document.getElementById("message");
 
     var user = {
         name: name.value,
@@ -13,10 +14,37 @@ function signup() {
         address: address.value
     }
 
-    console.log(user);
+    var users = JSON.parse(localStorage.getItem("users")) || [];
+    var userIndex = users.findIndex(val => val.email.toLowerCase() === user.email.toLowerCase());
+
+    if (userIndex === -1) {
+        users.push(user);
+        localStorage.setItem("users", JSON.stringify(users));
+        message.innerHTML = "SignUp Successful";
+
+        setTimeout(() => {
+            location.href = "login.html";
+        }, 1000);
+    }
+    else {
+        message.innerHTML = user.email + " use in another account";
+    }
+
+
 }
 
 function login() {
     var email = document.getElementById("email");
     var password = document.getElementById("password");
+
+    var user = {
+        email: email.value,
+        password: password.value,
+    }
+
+    var users = JSON.parse(localStorage.getItem("users")) || [];
+    // get indx
+    var currentUser = users.find(val =>
+        val.email.toLowerCase() === user.email.toLowerCase() && val.password === user.password
+    );
 }
