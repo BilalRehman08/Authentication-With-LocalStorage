9246caa94554aff61dd79ceeaefeca98f5c5017c
BilalRehman08 <bilalrehman080808@gmail.com>
2021-07-03 03:25:05 -0700

    first commit

--- app.js
+++ app.js
@@ -4,6 +4,16 @@ function signup() {
     var password = document.getElementById("password");
     var contact = document.getElementById("phone");
     var address = document.getElementById("address");
+
+    var user = {
+        name: name.value,
+        email: email.value,
+        password: password.value,
+        contact: contact.value,
+        address: address.value
+    }
+
+    console.log(user);
 }
 
 function login() {
