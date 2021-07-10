621e5966231dfb2988127c3301d0491a680dae33
BilalRehman08 <bilalrehman080808@gmail.com>
2021-07-10 06:46:46 -0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,36 @@
+(function ($) {
+
+	"use strict";
+
+	var fullHeight = function () {
+
+		$('.js-fullheight').css('height', $(window).height());
+		$(window).resize(function () {
+			$('.js-fullheight').css('height', $(window).height());
+		});
+
+	};
+	fullHeight();
+
+	$('#sidebarCollapse').on('click', function () {
+		$('#sidebar').toggleClass('active');
+	});
+
+})(jQuery);
+
+
+const posts = _ => {
+	var content = document.getElementById("posts");
+	var posts = document.createElement("li");
+	posts.id = "post";
+	posts.innerHTML = `
+	<div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="..." alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>`
+	content.appendChild(posts);
+}
